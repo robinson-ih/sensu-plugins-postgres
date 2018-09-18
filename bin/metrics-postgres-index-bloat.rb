31cf8abcd5409fa795ca95f10c236821ce5f303d
robinson-ih <robinson.a@introhive.com>
2018-09-18 15:25:59 +0530

    Update - Bloat check query

--- bin/metrics-postgres-index-bloat.rb
+++ bin/metrics-postgres-index-bloat.rb
@@ -68,10 +68,10 @@ class MetricsPostgresQuery < Sensu::Plugin::Metric::CLI::Graphite
          long: '--query QUERY',
          required: true,
          default: "WITH btree_index_atts AS (
-    SELECT nspname, 
-        indexclass.relname as index_name, 
-        indexclass.reltuples, 
-        indexclass.relpages, 
+    SELECT nspname,
+        indexclass.relname as index_name,
+        indexclass.reltuples,
+        indexclass.relpages,
         indrelid, indexrelid,
         indexclass.relam,
         tableclass.relname as tablename,
@@ -87,7 +87,7 @@ class MetricsPostgresQuery < Sensu::Plugin::Metric::CLI::Graphite
     ),
 index_item_sizes AS (
     SELECT
-    ind_atts.nspname, ind_atts.index_name, 
+    ind_atts.nspname, ind_atts.index_name,
     ind_atts.reltuples, ind_atts.relpages, ind_atts.relam,
     indrelid AS table_oid, index_oid,
     current_setting('block_size')::numeric AS bs,
@@ -102,7 +102,7 @@ index_item_sizes AS (
     JOIN btree_index_atts AS ind_atts ON pg_attribute.attrelid = ind_atts.indexrelid AND pg_attribute.attnum = ind_atts.attnum
     JOIN pg_stats ON pg_stats.schemaname = ind_atts.nspname
           -- stats for regular index columns
-          AND ( (pg_stats.tablename = ind_atts.tablename AND pg_stats.attname = pg_catalog.pg_get_indexdef(pg_attribute.attrelid, pg_attribute.attnum, TRUE)) 
+          AND ( (pg_stats.tablename = ind_atts.tablename AND pg_stats.attname = pg_catalog.pg_get_indexdef(pg_attribute.attrelid, pg_attribute.attnum, TRUE))
           -- stats for functional indexes
           OR   (pg_stats.tablename = ind_atts.index_name AND pg_stats.attname = pg_attribute.attname))
     WHERE pg_attribute.attnum > 0
@@ -113,59 +113,31 @@ index_aligned_est AS (
         relpages, relam, table_oid, index_oid,
         coalesce (
             ceil (
-                reltuples * ( 6 
-                    + maxalign 
+                reltuples * ( 6
+                    + maxalign
                     - CASE
                         WHEN index_tuple_hdr%maxalign = 0 THEN maxalign
                         ELSE index_tuple_hdr%maxalign
                       END
-                    + nulldatawidth 
-                    + maxalign 
+                    + nulldatawidth
+                    + maxalign
                     - CASE /* Add padding to the data to align on MAXALIGN */
                         WHEN nulldatawidth::integer%maxalign = 0 THEN maxalign
                         ELSE nulldatawidth::integer%maxalign
                       END
-                )::numeric 
+                )::numeric
               / ( bs - pagehdr::NUMERIC )
               +1 )
          , 0 )
       as expected
     FROM index_item_sizes
-),
-raw_bloat AS (
-    SELECT current_database() as dbname, nspname, pg_class.relname AS table_name, index_name,
-        bs*(index_aligned_est.relpages)::bigint AS totalbytes, expected,
-        CASE
-            WHEN index_aligned_est.relpages <= expected 
-                THEN 0
-                ELSE bs*(index_aligned_est.relpages-expected)::bigint 
-            END AS wastedbytes,
-        CASE
-            WHEN index_aligned_est.relpages <= expected
-                THEN 0 
-                ELSE bs*(index_aligned_est.relpages-expected)::bigint * 100 / (bs*(index_aligned_est.relpages)::bigint) 
-            END AS realbloat,
-        pg_relation_size(index_aligned_est.table_oid) as table_bytes,
-        stat.idx_scan as index_scans
+)
+SELECT sum(round(bs*(index_aligned_est.relpages)/1024/1024)::int) AS total_MB,
+       sum(round(bs*(index_aligned_est.relpages-expected)/1024/1024)::int) AS wasted_MB,
+       round(min(bs)*(sum(index_aligned_est.relpages)-sum(expected))::bigint * 100 / (min(bs)*(sum(index_aligned_est.relpages))::bigint)) AS bloat_pct
     FROM index_aligned_est
     JOIN pg_class ON pg_class.oid=index_aligned_est.table_oid
-    JOIN pg_stat_user_indexes AS stat ON index_aligned_est.index_oid = stat.indexrelid
-),
-format_bloat AS (
-SELECT dbname as database_name, nspname as schema_name, table_name, index_name,
-        round(realbloat) as bloat_pct, round(wastedbytes/(1024^2)::NUMERIC) as bloat_mb,
-        round(totalbytes/(1024^2)::NUMERIC,3) as index_mb,
-        round(table_bytes/(1024^2)::NUMERIC,3) as table_mb,
-        index_scans
-FROM raw_bloat
-)
--- final query outputting the bloated indexes
--- change the where and order by to change
--- what shows up as bloated
-SELECT *
-FROM format_bloat
-WHERE ( bloat_pct > 50 and bloat_mb > 10 )
-ORDER BY bloat_mb DESC;"
+      JOIN pg_stat_user_indexes AS stat ON index_aligned_est.index_oid = stat.indexrelid;"
 
   option :count_tuples,
          description: 'Count the number of tuples (rows) returned by the query',
@@ -217,8 +189,9 @@ ORDER BY bloat_mb DESC;"
 
     if config[:multirow] && !config[:count_tuples]
       res.values.each do |row|
-        output "#{config[:scheme]}.#{row[0]}.#{row[3]}.bloat_pct", row[4]
-        output "#{config[:scheme]}.#{row[0]}.#{row[3]}.bloat_mb", row[5]
+        output "#{config[:scheme]}.total_MB", row[0]
+        output "#{config[:scheme]}.wasted_MB", row[1]
+        output "#{config[:scheme]}.bloat_pct", row[2]
       end
     else
       output config[:scheme], value
