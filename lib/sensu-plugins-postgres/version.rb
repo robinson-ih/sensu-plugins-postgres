07236a14bf633ee5813c6967df55dd7305d55b14
Robinson <robinson@Robinsons-MacBook-Pro.local>
2019-01-30 20:09:32 +0530

    Prep 1.5.0

--- lib/sensu-plugins-postgres/version.rb
+++ lib/sensu-plugins-postgres/version.rb
@@ -1,8 +1,8 @@
 module SensuPluginsPostgres
   module Version
     MAJOR = 1
-    MINOR = 4
-    PATCH = 9
+    MINOR = 5
+    PATCH = 0
 
     VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
